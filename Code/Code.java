59e69f5458b959d7c21d4b2471641bd72dfbc7fb
Priyanshu Bharti <46714242+pseudoslayr@users.noreply.github.com>
2022-06-16 02:37:52 +0530

    Updated Files

--- Code/Code.java
+++
@@ -1,8 +0,0 @@
-import MyPack.NewPack;
-
-public class Code {
-  public static void main(String[] args) {
-    NewPack n = new NewPack();
-    n.display();
-  }
-}
--- Code/NewPack.java
+++
@@ -1,7 +0,0 @@
-package MyPack;
-
-public class NewPack {
-  public void display() {
-    System.out.println("This is the display method.");
-  }
-}
